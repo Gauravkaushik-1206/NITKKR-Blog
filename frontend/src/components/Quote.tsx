ea74e6a2738d18c15a8e568f3bf7a1198ff1cdcb
gaurav <gaurav@gauravs-MacBook-Air-2.local>
2025-02-26 14:44:36 +0530

    init frontend

---
+++ frontend/src/components/Quote.tsx
@@ -0,0 +1,19 @@
+export const Quote = () =>{
+    return (
+        <div className="flex flex-col bg-slate-200 h-screen justify-center">
+            <div className="flex justify-center">
+                <div className="max-w-md">
+                    <div className="text-2xl font-bold">
+                        "The customer service I received was exceptional. The support team went above and beyond to address my concerns."
+                    </div>
+                    <div className="text-xl font-semibold mt-4">
+                        Jules Winnield
+                    </div>
+                    <div className="text-sm font-light text-slate-400">
+                        CEO, Acme Inc
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
---
+++ frontend/src/pages/Signup.tsx
@@ -0,0 +1,16 @@
+import { Quote } from "../components/Quote";
+
+
+export function Signup() {
+  return (
+
+    <div className="grid grid-cols-2">
+        <div>
+            
+        </div>
+        <div className="invisible lg:visible">
+        <Quote></Quote>
+        </div>
+    </div>
+  )
+}
